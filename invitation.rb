c81a16bea9f754b0c1f75ebb1fab2ce05cf0b1d5
Lydia Tan <lydia.sydia@gmail.com>
2017-08-08 15:47:07 +0000

    created customizable invitations for KWK

--- invitation.rb
+++ invitation.rb
@@ -1,1 +1,5 @@
 #Code Here!
+#challenge 1
+gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. See you then!"
+
+puts gala_invitation.gsub("Sunday the 22nd of May 2015", "Saturday the 13th of May 2017")
--- metgala_invitation.rb
+++ metgala_invitation.rb
@@ -1,3 +1,20 @@
 #Code Here!
+day = "Friday"
+date = "26th"
+year = "2017"
+puts "What event will you be partaking in on #{day} the #{date} of May, #{year}?
+Options: 
+Museum of Modern Art gala
+Kode With Klossy amazing party
+Eat food all day"
 
-gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
+answer = gets.chomp
+
+
+gala_invitation = "You will be attending the #{answer} on #{day.upcase.downcase.capitalize} the #{date} of May #{year}. Festivities will be held in New York, NY. See you then!"
+
+# gala_invitation = "The Museum of Modern Art invites you to their annual gala on #{day.upcase.downcase.capitalize} the #{date} of May #{year}. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
+
+puts gala_invitation 
+
+#May 26th, 2017 (Friday).
